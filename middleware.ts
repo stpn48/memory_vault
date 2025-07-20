a894a763ffbd1892dd8b097411f825c0eb650bfe
stpn48 <133258815+stpn48@users.noreply.github.com>
2025-07-20 13:08:52 +0200

    protect routes

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,15 @@ import {
 } from "@convex-dev/auth/nextjs/server";
 
 const isSignInPage = createRouteMatcher(["/sign-in"]);
+const isHomePage = createRouteMatcher(["/"]);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
   if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
     return nextjsMiddlewareRedirect(request, "/");
+  }
+
+  if (isHomePage(request) && !(await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, "/sign-in");
   }
 });
 
